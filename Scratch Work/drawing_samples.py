5075735808ed010bdcb396772b267f163df33e89
Matthew Ross <matt@matthewpaulross.com>
2024-05-25 06:20:23 -0600

    Done through 5.5

---
+++ Scratch Work/drawing_samples.py
@@ -0,0 +1,8 @@
+import arcade
+
+arcade.open_window(600, 600, "Drawing Example")
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+arcade.start_render()
+arcade.finish_render()
+
+arcade.run()
